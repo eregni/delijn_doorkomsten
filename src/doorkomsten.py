3c02c71744cec850e8c0ba9560c0bff2d7e424bb
Edoardo Regni <eregni@pm.me>
2021-07-28 20:00:36 +0200

    added favorite

--- src/doorkomsten.py
+++ src/doorkomsten.py
@@ -28,13 +28,14 @@ FAVORITES = [
     ("sint-katelijne", 102700),
     ("metro groenplaats -> station", 103756),
     ("edenplein -> stad", 102831),
+    ("folklorelaan -> stad", 102848, 
     ("groenplaats bus", 102675),
     ("borkelstraat -> stad", 105449),
     ("amerlolaan -> stad", 101587),
     ("weegbreelaan -> stad", 109115),
     ("metro diamant -> groenplaats", 103377),
     ("metro astrid -> groenplaats", 103364),
-    ("centraal station -> melkmarkt", 102460)
+    ("centraal station -> melkmarkt", 102460),
 ]
 QUERY_LOG = "search.txt"
 API_CORE = 'https://www.delijn.be/rise-api-core/haltes/vertrekken'
