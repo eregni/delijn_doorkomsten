d54508b4e48e301b52aaeee1f96cbc7af5fd3d90
Edoardo Regni <eregni@pm.me>
2021-12-24 13:41:33 +0100

    wrapping up urwid stuff in class

--- src/main.py
+++ src/main.py
@@ -19,6 +19,8 @@ I used the old api because the new one (https://data.delijn.be/) needs much more
 """
 from signal import signal, SIGINT, SIGHUP
 from datetime import datetime, timedelta
+from typing import Union
+
 import urwid
 
 import delijnapi
@@ -27,6 +29,100 @@ import bookmarks
 
 QUERY_LOG = "search.txt"
 FAVORITES = [item for item in bookmarks.BOOKMARKS.items()]
+PROGRAM_TITLE = "\U0001F68B \U0001F68C \U0001F68B De lijn doorkomsten \U0001F68C \U0001F68B \U0001F68C"
+
+
+class Output(urwid.Padding):
+    """
+    Packaging of the urwid objects.
+    Methods control the appearance of the ui
+    """
+    def __init__(self):
+        self.div = urwid.Divider(div_char='-')
+
+        edit = urwid.Edit("")
+        self.input_user = UserInput(edit)
+
+        self.txt_output = urwid.Text(u"TEEEEEEST", align='left')  # TODO change to listbox
+        self.txt_info = urwid.Text(u"")
+
+        self.button_exit = urwid.Button(u"Afsluiten", on_press=exit_urwid)
+        self.button_bookmarks = urwid.Button(u"Favorieten", on_press=button_bookmarks_handler, user_data=self.txt_output)
+        self.button_new_search = urwid.Button(u"Nieuwe zoekopdracht", on_press=button_main_menu_handler)
+        self.buttons = urwid.Columns([], dividechars=1, focus_column=0)  # todo use property?
+        # todo add button <remove/filter> and <new search> in submenus
+
+        self.pile = urwid.Pile([self.txt_output])  # todo use property?
+
+        main_filler = urwid.Filler(self.pile, valign='top')
+        self.main_window = urwid.LineBox(urwid.Padding(main_filler, left=1), title=PROGRAM_TITLE)
+
+        super().__init__(self.main_window)
+        self.set_main_menu()
+
+    def _set_buttons(self, buttons: list[urwid.Button]):
+        self.buttons = urwid.Columns(
+            widget_list=[(len(btn.label) + 4, self.button_exit) for btn in buttons],
+            dividechars=1,
+            box_columns=buttons
+        )
+
+    def _set_pile(self, items: list[Union[urwid.Widget, tuple[str, urwid.Widget]]], focus_item: int):
+        self.pile = urwid.Pile(items, focus_item=focus_item)
+
+    def set_main_menu(self):
+        self.input_user.original_widget.set_caption(('bold', "Geef haltenummer of zoekterm"))
+        self.input_user.original_widget.set_edit_text("")
+        self.txt_output.set_text(get_bookmarks())
+        self._set_buttons([self.button_exit])
+        self._set_pile([
+            self.input_user,
+            self.div,
+            self.txt_output,
+            self.div,
+            ('pack', self.buttons)
+        ], focus_item=0)
+
+        if last_query:
+            self.txt_info.set_text(f"Druk op enter om terug '{last_query}' op te zoeken")
+            self.pile.widget_list.insert(0, self.txt_info)
+            self.pile.focus_item = 1
+
+    def set_search_halte(self):
+        raise NotImplementedError
+
+    def set_doorkomsten(self):
+        raise NotImplementedError
+
+    def set_filtered_doorkomsten(self):
+        raise NotImplementedError
+
+
+class UserInput(urwid.Padding):
+    """Handle user text input"""
+    def __init__(self, user_input: urwid.Edit):
+        super().__init__(user_input)
+
+    def keypress(self, size, key):
+        if key != 'enter':
+            return super(UserInput, self).keypress(size, key)
+
+        edit_text: str = self.original_widget.edit_text
+        if edit_text == '' and output.last_query:
+            edit_text = last_query
+
+        if edit_text.isdigit():
+            nr = int(edit_text)
+            if nr - 1 in range(len(bookmarks.BOOKMARKS)):
+                # todo update doorkomsten()
+                doorkomsten(FAVORITES[nr - 1][1], output)
+            else:
+                pass  # todo update doorkomsten()
+                doorkomsten(nr, output)
+        else:
+            search_halte(edit_text, output)
+
+        return super(UserInput, self).original_widget.set_edit_text("")
 
 
 def save_query(query_input: [str, int]) -> None:
@@ -44,7 +140,7 @@ def get_last_query() -> str:
         return ""
 
 
-def print_doorkomsten(lijnen: dict) -> None:
+def print_doorkomsten(lijnen: dict,  output_text: urwid.Text) -> None:
     """print parsed data in terminal"""
     text_color = Colors.Fg.lightblue
     print(f"\n{text_color}{lijnen['omschrijvingLang']} - haltenr: {lijnen['halteNummer']}")
@@ -86,10 +182,9 @@ def print_doorkomsten(lijnen: dict) -> None:
 
 def print_halte_search_results(table: dict, query: str, output_text: urwid.Text) -> None:
     """print parsed data in terminal from 'api_search_halte' function"""
-    output_text.set_text(('lightcyan', f"\n\"{query}\""))
+    text = [('lightcyan', f"\n\"{query}\": {len(table)} resultaten\n")]
     text_color = 'green'
     haltes = table['haltes']
-    text = []
     for index, halte in enumerate(haltes):
         lijn_nummers = ", ".join([lijn['lijnNummerPubliek'] for lijn in halte['lijnen']])
         bestemmingen = ", ".join(halte['bestemmingen'])
@@ -100,15 +195,15 @@ def print_halte_search_results(table: dict, query: str, output_text: urwid.Text)
     output_text.set_text(text)
 
 
-def print_bookmarks(output_text: urwid.Text) -> None:
-    """Print bookmark list"""
+def get_bookmarks() -> list[tuple[str, str]]:
+    """Give string + urwid markup from bookmark list"""
     stop_length = max([len(stop) for stop, _ in FAVORITES])
     bookmark_text = ""
     for index, (stop, nr) in enumerate(FAVORITES):
         index_text = f"{index + 1})"
         bookmark_text += f" {index_text:<4}{stop:<{stop_length}} ({nr})\n"
 
-    output_text.set_text([('lightcyan bold', "Favorieten\n"), ('lightcyan', bookmark_text)])
+    return [('lightcyan bold', "Favorieten\n"), ('lightcyan', bookmark_text)]
 
 
 def doorkomsten(halte_nummer: int, output_text: urwid.Text) -> None:
@@ -143,16 +238,18 @@ def doorkomsten(halte_nummer: int, output_text: urwid.Text) -> None:
                 line_filter = user_input
 
 
-def search_halte(query: str, output_text: urwid.Text, user_input: urwid.Edit) -> None:
+def search_halte(query: str, out: Output) -> None:
     data = delijnapi.api_search_halte(query)
     if data is None:
         return
     if not data['haltes']:
-        output_text.set_text(('red bold', "\nNiets gevonden.probeer een andere zoekterm"))
+        out.txt_output.set_text(('red bold', "\nNiets gevonden.probeer een andere zoekterm"))
     else:
         print_halte_search_results(data, query, txt_output)
         save_query(query)
-        user_input.set_caption(('bold', "Geef nummer: "))
+        out.input_user.set_caption(('bold', "Geef nummer: "))
+        test = out.pile.focus_position  # todo debug
+        # todo set focus on user_input
         return
         # todo continue here: change all button inputs/callbacks. Add buttons...
         user_input = input("\nKies nr. 0 = opnieuw beginnen: ")
@@ -164,8 +261,12 @@ def search_halte(query: str, output_text: urwid.Text, user_input: urwid.Edit) ->
             output_text.set_text(('red bold', "Ongeldige invoer"))
 
 
-def button_bookmarks_handler(button: urwid.Button, text: urwid.Text) -> None:
-    print_bookmarks(text)
+def button_bookmarks_handler(button: urwid.Button, out: Output) -> None:
+    out.txt_info.set_text(get_bookmarks())
+
+
+def button_main_menu_handler(button: urwid.Button, out: Output):
+    out.set_main_menu()
 
 
 def exit_urwid(*args):
@@ -180,30 +281,6 @@ def unhandled_input_handler(key):
 def signal_handler(*args) -> None:
     """Handler for SIGINT signal"""
     exit_urwid("signal")
-
-
-class UserInput(urwid.Padding):
-    def keypress(self, size, key):
-        if key == 'enter':
-            edit_text: str = self.original_widget.edit_text
-            if edit_text == '' and last_query:
-                edit_text = last_query
-
-            if edit_text.isdigit():
-                nr = int(edit_text)
-                if nr - 1 in range(len(bookmarks.BOOKMARKS)):
-                    # todo update doorkomsten()
-                    doorkomsten(FAVORITES[nr - 1], txt_output)
-                else:
-                    pass  # todo update doorkomsten()
-                    doorkomsten(nr, txt_output)
-            else:
-                search_halte(edit_text, txt_output, input_user.original_widget)
-
-            return super(UserInput, self).original_widget.set_edit_text("")
-
-        else:
-            return super(UserInput, self).keypress(size, key)
 
 
 if __name__ == '__main__':
@@ -226,47 +303,8 @@ if __name__ == '__main__':
         """
     signal(SIGINT, signal_handler)
     signal(SIGHUP, signal_handler)
-
-    # urwid layout + callbacks
-    program_title = "\U0001F68B \U0001F68C \U0001F68B De lijn doorkomsten \U0001F68C \U0001F68B \U0001F68C"
-    div = urwid.Divider(div_char='-')
-    edit = urwid.Edit(('bold', u"Geef haltenummer of zoekterm: "))
-    txt_output = urwid.Text(u"", align='left')  # TODO FIRST turn into scrollList before continuing adjusting functions
-    txt_info = urwid.Text(u"")
-    input_user = UserInput(edit)
-    # urwid.connect_signal(input_user, 'change', input_handler, user_args=[txt_info, txt_output])
-    button_exit = urwid.Button(u"Afsluiten", on_press=exit_urwid)
-    button_bookmarks = urwid.Button(u"Favorieten", on_press=button_bookmarks_handler, user_data=txt_output)
-    # todo add button <remove/filter> and <new search> in submenus
-    button_column = urwid.Columns([
-        # width -> len(Button.label + 4 button chars. 'pack' didn't work very well...
-        (len(button_exit.label) + 4, button_exit),t
-        (len(button_bookmarks.label) + 4, button_bookmarks)
-    ], dividechars=1, box_columns=[
-        button_exit,
-        button_bookmarks
-    ])
-
-    pile = urwid.Pile([
-        input_user,
-        div,
-        txt_output,
-        div,
-        ('pack', button_column)
-    ], focus_item=1)
-
-    main_window = urwid.Filler(pile, valign='top')
-    main_window = urwid.LineBox(urwid.Padding(main_window, left=1), title=program_title)
-    loop = urwid.MainLoop(main_window, unhandled_input=unhandled_input_handler, palette=PALETTE)
-
-    # program
     last_query = get_last_query()
-    if last_query:
-        txt_info.set_text(f"Druk op enter om terug '{last_query}' op te zoeken")
-        pile.widget_list.insert(0, txt_info)
-        pile.focus_item = 1
-
-    print_bookmarks(txt_output)
-
+    output = Output()
+    loop = urwid.MainLoop(output.original_widget, unhandled_input=unhandled_input_handler, palette=PALETTE)
     loop.run()
     exit(0)
